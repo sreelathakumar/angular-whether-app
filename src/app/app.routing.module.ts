e50417b0e67194116474515efc07bfa80c932216
sreelathakumar <sreelatha1288@gmail.com>
2022-05-25 13:16:13 +0530

    Initial commit

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes , RouterOutlet } from '@angular/router';
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
